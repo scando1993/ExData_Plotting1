4bc5ed6740bcdc10f1eee95fc3ac017244703483
Kevin Cando <scando@fiec.espol.edu.ec>
2015-06-07 17:25:31 -0500

    First Commit with the code for the assigment

---
+++ plot4.R
@@ -0,0 +1,12 @@
+plot4 <- function(file){
+  #Creates the dataset and charges its values
+  dataSet<-read.table(file = file,header = TRUE,sep = ";",colClasses = c("character","character","character","character","character","character","character","character","character"))
+  dataSet <- dataSet[dataSet$Date %in% c("1/2/2007","2/2/2007"),]
+  
+  #Open a PNG device and creates plot1.png in working directory
+  png(filename = "plot4.png",width = 480,height = 480)
+  #Creates the histogram 
+  hist(as.numeric(dataSet$Global_active_power), col = "red", main = "Global active Power", xlab = "Global active Power (kilowatts)")
+  #Closes the device
+  dev.off()
+}
